6713bd9a27c7338e5b246537c4e5b54e35af0179
RSIT <rs@ricosonntagit.com>
2021-07-26 14:52:34 +0200

    version 8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
 
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+
+import { DynTableComponent } from './components/dyn-table/dyn-table.component';
+import { DynColumnComponent } from './components/dyn-table/dyn-column.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, MaterialModule, BrowserAnimationsModule ],
+  declarations: [ AppComponent, HelloComponent, DynTableComponent, DynColumnComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/components/dyn-table/dyn-table.component.ts
@@ -0,0 +1,59 @@
+import {
+    AfterContentInit, Component, ContentChild,
+    ContentChildren, EventEmitter, Input,
+    OnChanges, OnInit, ViewChildren,
+    Output, QueryList, ViewChild
+} from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort, MatSortable, MatSortHeader } from '@angular/material/sort';
+import { MatColumnDef, MatHeaderRowDef, MatRowDef, MatTable, MatTableDataSource } from '@angular/material/table';
+import { DynColumnComponent } from './dyn-column.component';
+
+@Component({
+  selector: 'app-dyn-table',
+  templateUrl: './dyn-table.component.html'
+})
+export class DynTableComponent<T> implements OnChanges, AfterContentInit {
+
+  @ContentChildren(DynColumnComponent) dynColumns: QueryList<DynColumnComponent<T>>;
+  @ContentChildren(MatColumnDef) columnDefs: QueryList<MatColumnDef>;
+
+  @ContentChild(MatHeaderRowDef, { static: true }) headerRowDef: MatHeaderRowDef;
+  @ContentChildren(MatRowDef) rowDefs: QueryList<MatRowDef<T>>;
+
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ContentChild(MatSort, { static: true }) sort: MatSort;
+
+  @ViewChild(MatTable, { static: true }) table: MatTable<T>;
+
+  @Input() displayedColumns: any;
+  @Input() data: any;
+
+  public dataSource = new MatTableDataSource([]);
+
+  constructor() { }
+
+  ngOnChanges() {
+    if (this.data) {
+      this.setData(this.data);
+    }
+  }
+
+  ngAfterContentInit() {
+    // console.log(this.dynColumns, this.columnDefs);
+    this.dynColumns.forEach(dynColumn => this.table.addColumnDef(dynColumn.columnDef));
+    this.columnDefs.forEach(columnDef => this.table.addColumnDef(columnDef));
+
+    // this.rowDefs.forEach(rowDef => this.table.addRowDef(rowDef));
+    // this.table.addHeaderRowDef(this.headerRowDef);
+  }
+
+  setData(data) {
+    if (Array.isArray(data)) {
+      this.dataSource.data = data;
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    }
+  }
+
+}
